cda1081bf38c0f6ae4aa2fa40f90557c80db89d8
Stacey Svetlichnaya <stacey@wandb.com>
2020-07-23 00:06:53 -0700

    add custom mode

--- main.py
+++ main.py
@@ -156,7 +156,7 @@ if __name__ == '__main__':
 
     # misc
     parser.add_argument('--mode', type=str, required=True,
-                        choices=['train', 'sample', 'eval', 'align'],
+                        choices=['train', 'sample', 'custom', 'eval', 'align'],
                         help='This argument is used in solver')
     parser.add_argument('--num_workers', type=int, default=4,
                         help='Number of workers used in DataLoader')
